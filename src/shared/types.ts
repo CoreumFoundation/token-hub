53b295da0ffdee82ee570f98415c8322d66d5057
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-04-22 17:06:06 +0300

    feat: add types

---
+++ src/shared/types.ts
@@ -0,0 +1,38 @@
+export enum FooterIconType {
+  X = 'twitter',
+  Instagram = 'instagram',
+  Telegram = 'telegram',
+  Discord = 'discord',
+  Youtube = 'youtube',
+  Medium = 'medium',
+};
+
+export enum ButtonType {
+  Primary = 'primary',
+  Secondary = 'secondary',
+};
+
+export enum ButtonIconType {
+  Check = 'check',
+  Wallet = 'wallet',
+  Token = 'token',
+  Plus = 'plus',
+  Send = 'send',
+};
+
+export enum ButtonIconColorScheme  {
+  Primary = '#21262E',
+  Secondary = '#25D695',
+  Disabled = '#2B3138',
+};
+
+export enum DropdownType {
+  Primary = 'primary',
+  Secondary = 'secondary',
+};
+
+export enum GeneralIconType {
+  Coreum = 'coreum',
+  Network = 'network',
+  CheckMark = 'check_mark',
+}

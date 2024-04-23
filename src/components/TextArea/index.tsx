73e2b60da7489a2252e74d04ac954becb279115d
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-04-23 19:40:46 +0300

    feat: add TextArea component

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,72 @@
+import { GeneralIcon } from "@/assets/GeneralIcon";
+import { GeneralIconType } from "@/shared/types";
+import classNames from "classnames";
+import { ChangeEvent, FC, useCallback, useMemo, useState } from "react";
+
+interface TextAreaProps {
+  label: string;
+  value: string;
+  rows: number;
+  onChange: (value: string) => void;
+  placeholder: string;
+  disabled?: boolean;
+  id?: string;
+  error?: string;
+}
+
+export const TextArea: FC<TextAreaProps> = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+  disabled = false,
+  id,
+  error,
+  rows,
+}) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const textAreaCx = useMemo(() => {
+    return classNames('flex items-center w-full gap-3 justify-between border border-[#1B1D23] py-3 px-4 rounded-[10px]', {
+      'border-[#25D695]': isFocused,
+      'hover:border-[#2B3138]': !isFocused && !disabled && !error?.length,
+      'border-[#17191E]': !isFocused && !disabled && !error?.length && value.length,
+      'border-[#DE0F3E]': error?.length,
+      'border-transparent': disabled,
+    });
+  }, [disabled, error?.length, isFocused, value.length]);
+
+  const handleChangeInput = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e.target.value);
+  }, []);
+
+  return (
+    <div className="flex flex-col w-full gap-2 relative">
+      <label
+        htmlFor={id || label.toLowerCase()}
+        className="block text-sm text-[#868991] font-noto-sans"
+      >
+        {label}
+      </label>
+      <div className={textAreaCx}>
+        <textarea
+          rows={rows}
+          name={id || label.toLowerCase()}
+          id={id}
+          className="flex-1 w-full bg-transparent text-[#EEE] placeholder:text-[#5E6773] outline-none shadow-sm resize-none"
+          placeholder={placeholder}
+          disabled={disabled}
+          value={value}
+          onChange={handleChangeInput}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </div>
+      {error && (
+        <div className="absolute bottom-0 flex items-center gap-2 -mb-7 text-xs text-[#DE0F3E]">
+          <GeneralIcon type={GeneralIconType.Error} /> Error Message
+        </div>
+      )}
+    </div>
+  )
+}

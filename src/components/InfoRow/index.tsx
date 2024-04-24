dc379567106d7dff56d1952ebfbfa129265e7d00
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-04-24 17:47:18 +0300

    feat: add InfoRow component

---
+++ src/components/InfoRow/index.tsx
@@ -0,0 +1,27 @@
+import { GeneralIcon } from "@/assets/GeneralIcon";
+import { GeneralIconType } from "@/shared/types";
+import { FC } from "react";
+
+interface InfoRowProps {
+  label: string;
+  value: string | React.ReactNode;
+}
+
+export const InfoRow: FC<InfoRowProps> = ({
+  label,
+  value,
+}) => {
+  return (
+    <div className="flex items-center w-full justify-between gap-4">
+      <div className="flex items-center gap-1.5">
+        <div className="text-sm capitalize text-[#5E6773]">
+          {label}
+        </div>
+        <GeneralIcon type={GeneralIconType.Info} className="group cursor-pointer" pathClassName="group-hover:fill-[#eee]" />
+      </div>
+      <div className="flex items-center text-[#EEE] font-noto-sans text-base">
+        {value}
+      </div>
+    </div>
+  );
+};

9775a06762c5fde10037262d32398987b218f220
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-05-07 19:31:22 +0300

    fix: update the description for ft manage

--- src/containers/FungibleTokenManage/index.tsx
+++ src/containers/FungibleTokenManage/index.tsx
@@ -117,7 +117,7 @@ export const FungibleTokenManage = () => {
     <div className="flex flex-col gap-10">
       <MessageBox>
         <ul className="list-disc text-[#868991] text-sm font-normal ml-5">
-          <li>View and manage the Smart Tokens you created or own.</li>
+          <li>View and manage the Smart Tokens you created.</li>
           <li>There are many ways to get on-chain data. Here we simply query a <Link className="text-[#25D695] underline font-medium" href={`https://full-node.${network}-1.coreum.dev:1317`} target="_blank">public REST server</Link> that expose different endpoints to query the blockchain.</li>
         </ul>
       </MessageBox>

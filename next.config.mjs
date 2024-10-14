b668be91381405d2a230194ca0aa222e0a7a01f9
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-10-14 14:53:11 +0300

    fix: update config to avoid usage of dangerouslyAllowSVG

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   output: "standalone",
   images: {
     minimumCacheTTL: 900,
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*',
-        port: '',
-      }
-    ],
   }
 }
 

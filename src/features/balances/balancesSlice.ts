2d4cb1c909028519d3a411e291e997f2a511bd24
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2025-01-17 12:01:55 +0200

    feat: rework logic to handle balances with usage of rest api

--- src/features/balances/balancesSlice.ts
+++ src/features/balances/balancesSlice.ts
@@ -1,15 +1,82 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Coin } from "@cosmjs/amino";
-import { Token } from '@/shared/types';
+import { Network, Token } from '@/shared/types';
+import axios, { AxiosResponse } from 'axios';
+import { fetchSecondaryCurrenciesInfo } from '../currencies/currenciesSlice';
+
+interface FetchBalancesByAccountArgs {
+  account: string;
+  network: Network;
+}
+
+export const fetchBalancesByAccount = createAsyncThunk(
+  'balances/fetchByAccount',
+  async ({ account, network }: FetchBalancesByAccountArgs, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const currencies = (state as any).currencies.list;
+
+    let balancesToSet = [];
+    try {
+      const balancesRequestUrl = `https://full-node.${network}-1.coreum.dev:1317/cosmos/bank/v1beta1/balances/${account}`;
+      const {
+        data: {
+          pagination: { total: balancesTotal },
+          balances,
+        },
+      }: AxiosResponse<{ pagination: { total: string }; balances: Coin[]; }> = await axios.get(balancesRequestUrl);
+
+      balancesToSet = balances;
+
+      if (Number(balancesTotal) > balances.length) {
+        const {
+          data: {
+            balances: allBalances,
+          }
+        }: AxiosResponse<{ balances: Coin[] }> = await axios.get(`${balancesRequestUrl}&pagination.limit=${balancesTotal}`);
+
+        balancesToSet = allBalances;
+      }
+
+      const userIssuedBalances: any[] = [];
+      const userOtherBalances: Coin[] = [];
+
+      balances.forEach((token: Coin) => {
+        const isIssued = currencies.find((currency: Token) => currency.denom === token.denom);
+
+        if (isIssued) {
+          userIssuedBalances.push({
+            ...isIssued,
+            amount: token.amount,
+          });
+        } else {
+          userOtherBalances.push(token);
+        }
+      });
+
+      thunkAPI.dispatch(setBalances(userIssuedBalances));
+      thunkAPI.dispatch(fetchSecondaryCurrenciesInfo({ network, currencies: userOtherBalances }));
+
+      return balancesToSet;
+    } catch (error) {
+      return [];
+    }
+  },
+);
 
 export interface BalancesState {
   list: Token[];
   shouldRefetch: boolean;
+  isLoading: boolean;
+  restBalances: any[];
+  isFetched: boolean;
 }
 
 export const initialBalancesState: BalancesState = {
+  isLoading: false,
   list: [],
   shouldRefetch: false,
+  restBalances: [],
+  isFetched: false,
 };
 
 const balancesSlice = createSlice({
@@ -22,6 +89,21 @@ const balancesSlice = createSlice({
     shouldRefetchBalances(state, action: PayloadAction<boolean>) {
       state.shouldRefetch = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBalancesByAccount.pending, (state) => {
+      state.isLoading = true;
+    })
+    builder.addCase(fetchBalancesByAccount.rejected, (state, action) => {
+      state.restBalances = action.payload as any[];
+      state.isLoading = false;
+      state.isFetched = true;
+    })
+    builder.addCase(fetchBalancesByAccount.fulfilled, (state, action) => {
+      state.restBalances = action.payload;
+      state.isLoading = false;
+      state.isFetched = true;
+    })
   },
 });
 
--- src/hooks/useBalances.ts
+++ src/hooks/useBalances.ts
@@ -1,66 +1,30 @@
-import { setBalances, shouldRefetchBalances } from "@/features/balances/balancesSlice";
-import { fetchSecondaryCurrenciesInfo } from "@/features/currencies/currenciesSlice";
-import { Token } from "@/shared/types";
+import { fetchBalancesByAccount, shouldRefetchBalances } from "@/features/balances/balancesSlice";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { Coin } from "@cosmjs/amino";
-import { useBalances } from "graz";
 import { useEffect } from "react";
 
 export const useAccountBalances = () => {
-  const networkInfo = useAppSelector(state => state.general.currentNetworkInfo);
   const currencies = useAppSelector(state => state.currencies.list);
   const account = useAppSelector(state => state.general.account);
   const isConnected = useAppSelector(state => state.general.isConnected);
   const shouldRefetch = useAppSelector(state => state.balances.shouldRefetch);
   const network = useAppSelector(state => state.general.network);
-  const isCurrenciesLoading = useAppSelector(state => state.currencies.isLoading);
-  const balancesRedux = useAppSelector(state => state.balances.list);
 
   const dispatch = useAppDispatch();
 
-  const { data: balances, refetch, isLoading } = useBalances({
-    chainId: networkInfo.chainId,
-    bech32Address: account,
-  });
+  const balances = useAppSelector(state => state.balances.restBalances);
+  const isBalancesFetched = useAppSelector(state => state.balances.isFetched);
+  const isLoading = useAppSelector(state => state.balances.isLoading);
 
   useEffect(() => {
-    if (isConnected) {
-      if (balances && !isCurrenciesLoading && !balancesRedux.length) {
-        const userIssuedBalances: any[] = [];
-        const userOtherBalances: Coin[] = [];
-        balances.forEach((token: Coin) => {
-          const isIssued = currencies.find((currency: Token) => currency.denom === token.denom);
-
-          if (isIssued) {
-            userIssuedBalances.push({
-              ...isIssued,
-              amount: token.amount,
-            });
-          } else {
-            userOtherBalances.push(token);
-          }
-        });
-
-        dispatch(setBalances(userIssuedBalances));
-        dispatch(fetchSecondaryCurrenciesInfo({ network, currencies: userOtherBalances }));
-      }
-    } else {
-      dispatch(setBalances([]));
+    if (isConnected && account.length && !isLoading && currencies.length && !isBalancesFetched) {
+      dispatch(fetchBalancesByAccount({ account, network }));
     }
-  }, [
-    currencies,
-    balances,
-    isConnected,
-    network,
-    isCurrenciesLoading,
-    isLoading,
-    balancesRedux.length,
-  ]);
+  }, [account, currencies, isBalancesFetched, isConnected, isLoading, network]);
 
   useEffect(() => {
-    if (shouldRefetch && !isLoading) {
-      refetch();
+    if (shouldRefetch && !isLoading && account.length) {
+      dispatch(fetchBalancesByAccount({ account, network }));
       dispatch(shouldRefetchBalances(false));
     }
-  }, [shouldRefetch, refetch, isLoading]);
+  }, [shouldRefetch, isLoading, dispatch, account, network]);
 };

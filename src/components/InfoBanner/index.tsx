a8a04d94e1faf15a084b3ac11a687ed1a351dc31
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2026-02-05 21:57:46 +0200

    fix: hardcode banner value

--- src/components/InfoBanner/index.tsx
+++ src/components/InfoBanner/index.tsx
@@ -56,7 +56,7 @@ const getBannerContent = (type: BannerType): BannerContent | null => {
 export const InfoBanner = () => {
   const [isOpen, setIsOpen] = useState(true);
 
-  const bannerType = (process.env.NEXT_PUBLIC_BANNER_TYPE as BannerType) || 'none';
+  const bannerType = (process.env.NEXT_PUBLIC_BANNER_TYPE as BannerType) || 'announce';
   const content = getBannerContent(bannerType);
 
   if (!isOpen || !content) {

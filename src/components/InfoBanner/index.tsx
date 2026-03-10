e250cd1a2c0b46faa0c6abed9f313877d3b544d1
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2026-03-10 14:30:48 +0200

    feat: update banner state

--- src/components/InfoBanner/index.tsx
+++ src/components/InfoBanner/index.tsx
@@ -56,9 +56,7 @@ const getBannerContent = (type: BannerType): BannerContent | null => {
 export const InfoBanner = () => {
   const [isOpen, setIsOpen] = useState(true);
 
-  const bannerType = (process.env.NEXT_PUBLIC_BANNER_TYPE as BannerType) || 'approved';
-
-  console.log('NEXT_PUBLIC_BANNER_TYPE: ',process.env.NEXT_PUBLIC_BANNER_TYPE)
+  const bannerType = (process.env.NEXT_PUBLIC_BANNER_TYPE as BannerType) || 'executed';
   const content = getBannerContent(bannerType);
 
   if (!isOpen || !content) {
